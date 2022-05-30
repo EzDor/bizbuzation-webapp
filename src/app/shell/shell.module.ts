cbd94ab31c6c80cbf6a10b321e2790686dec9317
Dor Anidjer <danidjer@salesforce.com>
2022-05-30 10:00:38 +0300

    Add buttons wrapper to navbar

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule } from "@angular/router";
 import { SharedModule } from "src/app/shared/shared.module";
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -18,8 +18,8 @@ import { SideBarComponent } from './side-bar/side-bar.component';
     SideBarComponent
   ],
   imports: [
-    CommonModule,
-    SharedModule
+    SharedModule,
+    RouterModule
   ]
 })
 export class ShellModule { }
