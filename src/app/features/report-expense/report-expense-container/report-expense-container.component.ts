cc5c41edf7a10505b573895cc189ff5585d1952f
Yosef mare <yosefmare108@gmail.com>
2023-01-10 18:04:23 +0200

    fix the task

--- src/app/features/report-expense/report-expense-container/report-expense-container.component.ts
+++ src/app/features/report-expense/report-expense-container/report-expense-container.component.ts
@@ -88,17 +88,18 @@ export class ReportExpenseContainerComponent implements OnInit, OnDestroy {
 
 	public addReportExpense(reportExpenseItem): void {
 
+    this.editReportExpense(reportExpenseItem)
+
+	}
+
+	public editReportExpense(reportExpenseItem: ReportExpenseItem): void {
+		console.log(reportExpenseItem);
     const dialogRef = this.dialog.open(ReportExpenseCreateOrEditDialogComponent, {
 			width: '650px',
 			data: reportExpenseItem,
 		});
 
 		this.subscribeSubmitEvent(dialogRef);
-
-	}
-
-	public editReportExpense(reportExpenseItem: ReportExpenseItem): void {
-		console.log(reportExpenseItem);
 	}
 
 	private subscribeSubmitEvent(dialogRef: MatDialogRef<ReportExpenseCreateOrEditDialogComponent>) {
